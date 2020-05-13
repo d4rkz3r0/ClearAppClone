cdd5c24c9ee4c2deecab825ad53da8eb07a2e751
d4rkz3r0 <d4rkz3r0@users.noreply.github.com>
2020-05-13 10:13:44 -0400

    Initial Commit

---
+++ ClearAppClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClearAppClone
+//
+//  Created by Steve Kerney on 5/13/20.
+//  Copyright © 2020 Steve Kerney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
