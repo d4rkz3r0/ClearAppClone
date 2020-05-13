dafd6af12f486cbccc915d88e538812870b1ffbd
d4rkz3r0 <d4rkz3r0@users.noreply.github.com>
2020-05-13 19:14:03 -0400

    Add checkmark accessory to cells for selected state.

--- ClearAppClone/TodoListViewController.swift
+++ ClearAppClone/TodoListViewController.swift
@@ -23,11 +23,21 @@ extension TodoListViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: kTodoCellReuseIdentifier, for: indexPath)
         cell.textLabel!.text = items[indexPath.row]
+        cell.accessoryType = .none
         return cell
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return items.count
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard let selectedCell = tableView.cellForRow(at: indexPath) else {
+            return
+        }
+        selectedCell.accessoryType = selectedCell.accessoryType == .checkmark ? .none : .checkmark
+
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
